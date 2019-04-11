d4641e3ef8680229c2ad5290089f33a82a717d1f
d0dders <dodders@outlook.com>
2019-04-11 21:55:33 +0100

    Add project files.

---
+++ ToDoList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ToDo> toDoList = new List<ToDo>();
+
+            while (true)
+            {
+                AddToDo(toDoList);
+
+                Console.WriteLine($"There are {toDoList.Count} To Do's on your list");
+            }
+        }
+
+        private static void AddToDo(List<ToDo> toDoList)
+        {
+            Console.WriteLine("Enter a task description:");
+            string description = Console.ReadLine();
+            Console.WriteLine("Enter a due date:");
+            DateTime dueDate = DateTime.Parse(Console.ReadLine());
+            ToDo toDo = new ToDo(description, dueDate);
+            toDoList.Add(toDo);
+        }
+    }
+}
---
+++ ToDoList/ToDo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class ToDo
+    {
+        public enum State
+        {
+            [Description("Not started")]
+            Incomplete,
+            [Description("In-progress")]
+            InProgress,
+            [Description("Completed")]
+            Completed
+
+        }
+
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CompletedDate { get; set; }
+
+        public ToDo(string description, DateTime dueDate)
+        {
+            Description = description;
+            DueDate = dueDate;
+            State state = State.Incomplete;
+        }
+    }
+}
